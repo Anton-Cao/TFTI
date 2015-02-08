a01f55e13384fb753756259deb504f592cdc2d83
Anton-Cao <cao.anton@gmail.com>
2015-02-08 11:54:05 -0800

    picture

--- js/party.js
+++ js/party.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   Parse.initialize("VLQWsrqnc7SxqENlnzi1zsoBLCJZIhLbdPIFGH05", "Oj0yEW977q1A0174sWCTk8IbzP5sCcoWZO1pKpB4");
-
+  var currentUser = Parse.User.current();
+  if(currentUser){
+    $('.notLoggedIn').fadeOut(0);
+    $('.loggedIn').fadeIn(0);
+    $('#usernameText').text(currentUser.get("username"));
+  }else{
+    $('.notLoggedIn').fadeIn(0);
+    $('.loggedIn').fadeOut(0);
+  }
+  
   var cookie = document.cookie;
   cookie = cookie.substring(6,cookie.length);
   var party;
@@ -19,7 +28,7 @@ $(document).ready(function(){
       // Do something with the returned Parse.Object values
       party = results[0];
       var photo = party.get("Image");
-      $("#pic_party").src = photo.url();
+      $("#pic_party").attr("src",photo.url());
       console.log(photo.url());
 
       $('#party_name').text(party.get("Title"));
